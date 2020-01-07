6b66da75f42a591e6442db3dc3738d1a6a6b921c
jacob <jacobbrookswork@gmail.com>
2020-01-06 19:26:19 -0500

    chat window and message components

---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="ChatMessageRoot" style="width:100%;display:inline-block">
+        <div :style="messageType">
+            {{message.body}}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ChatMessage',
+        props: ["message"],
+        computed: {
+            messageType: function () {
+                if (this.message.from == "user") {
+                    //console.log("user message");
+                    return {
+                        backgroundColor: "blue",
+                        float: "right"
+                    }
+                } else if (this.message.from == "app") {
+                    //console.log("app message");
+                    return {
+                        backgroundColor: "red",
+                        float: "left"
+                    }
+                } else {
+                    return {}
+                }
+            }
+        },
+        methods: {
+        },
+        mounted: function () {
+            //console.log(this.message);
+        }
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    #ChatMessageRoot{
+        height: auto;
+        width:auto;
+    }
+</style>
+
